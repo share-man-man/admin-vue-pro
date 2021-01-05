ea84ae83b6f5e93ccd14ab93e1cfe2d26026d86a
GrainFull <651487447@qq.com>
2021-01-05 18:47:12 +0800

    多个双向绑定的props

---
+++ src/components/ClickButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <input :value="title" @input="onInput" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  naem: "ClickButton",
+  props: {
+    title: {
+      type: [String],
+      default: () => ""
+    },
+    message: {
+      type: [String],
+      default: () => ""
+    },
+    titleModifiers: {
+      default: () => ({
+        daxie: false
+      })
+    }
+  },
+  emits: ["update:title"],
+  data() {
+    return {
+      total: 0
+    };
+  },
+  methods: {
+    onInput(e: any) {
+      let value = e?.target?.value;
+      if (this.titleModifiers?.daxie) {
+        value = `${value[0].toUpperCase()}${value.slice(1)}`;
+      }
+      this.$emit("update:title", value);
+    }
+  }
+});
+</script>
