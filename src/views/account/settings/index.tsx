92c060a030c141d3250dd006ac00407d6ebdbcbd
舒晓满 <651487447@qq.com>
2021-03-10 22:59:36 +0800

    个人设置添加响应式

--- src/views/account/settings/index.tsx
+++ src/views/account/settings/index.tsx
@@ -1,15 +1,21 @@
 import { PageContent } from "@/components/Page";
+import { LayoutState } from "@/store/modules/layout";
 import { Card, Menu } from "ant-design-vue";
-import { computed, defineComponent, KeepAlive, reactive, VNode } from "vue";
+import { computed, defineComponent, KeepAlive, ref, VNode } from "vue";
 import { RouterLink, RouterView, useRoute } from "vue-router";
+import { useStore } from "vuex";
 import style from "./style.module.less";
 
 export default defineComponent({
   setup() {
     const route = useRoute();
+    const store = useStore<{ layout: LayoutState }>();
     // 路由改变时，重新加载选中项
     const routePath = computed(() => route.path);
-    const menuList = reactive([
+    const mobileClass = computed(() =>
+      store.state.layout.isMobile ? style["mobile"] : undefined
+    );
+    const menuList = ref([
       {
         key: "/account/settings/base",
         text: "基础设置"
@@ -26,10 +32,13 @@ export default defineComponent({
     return () => (
       <PageContent>
         <Card>
-          <div class={style["main"]}>
-            <div class={style["left"]}>
-              <Menu selectedKeys={[routePath.value]}>
-                {menuList.map(i => (
+          <div class={[style["main"], mobileClass.value]}>
+            <div class={[style["left"], mobileClass.value]}>
+              <Menu
+                mode={mobileClass.value ? "horizontal" : "inline"}
+                selectedKeys={[routePath.value]}
+              >
+                {menuList.value.map(i => (
                   <Menu.Item key={i.key}>
                     <RouterLink to={i.key}>{i.text}</RouterLink>
                   </Menu.Item>
