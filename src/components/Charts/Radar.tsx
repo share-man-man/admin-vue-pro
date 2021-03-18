4977c66054a3175d57691706c08426e44e82ebae
GrainFull <651487447@qq.com>
2021-03-18 23:17:20 +0800

    引入g2plot图表；完成工作台页面；

---
+++ src/components/Charts/Radar.tsx
@@ -0,0 +1,71 @@
+import { defineComponent, onMounted } from "vue";
+import { Radar, RadarOptions } from "@antv/g2plot";
+
+export default defineComponent({
+  setup() {
+    const data = [
+      { item: "Design", user: "a", score: 70 },
+      { item: "Design", user: "b", score: 30 },
+      { item: "Design", user: "c", score: 40 },
+      { item: "Development", user: "a", score: 60 },
+      { item: "Development", user: "b", score: 70 },
+      { item: "Development", user: "c", score: 10 },
+      { item: "Marketing", user: "a", score: 50 },
+      { item: "Marketing", user: "b", score: 60 },
+      { item: "Marketing", user: "c", score: 30 },
+      { item: "Users", user: "a", score: 40 },
+      { item: "Users", user: "b", score: 50 },
+      { item: "Users", user: "c", score: 80 },
+      { item: "Test", user: "a", score: 60 },
+      { item: "Test", user: "b", score: 70 },
+      { item: "Test", user: "c", score: 50 }
+    ];
+
+    const config: RadarOptions = {
+      data,
+      xField: "item",
+      yField: "score",
+      seriesField: "user",
+      meta: {
+        score: {
+          alias: "分数",
+          min: 0,
+          max: 80
+        }
+      },
+      xAxis: {
+        line: null,
+        tickLine: null,
+        grid: {
+          line: {
+            style: {
+              lineDash: null
+            }
+          }
+        }
+      },
+      yAxis: {
+        line: null,
+        tickLine: null,
+        grid: {
+          line: {
+            type: "line",
+            style: {
+              lineDash: null
+            }
+          }
+        }
+      },
+      // 开启辅助点
+      point: {
+        size: 2
+      }
+    };
+
+    onMounted(() => {
+      const radarPlot = new Radar("radarContainer", config);
+      radarPlot.render();
+    });
+    return () => <div id="radarContainer"></div>;
+  }
+});
--- src/views/workspace/index.tsx
+++ src/views/workspace/index.tsx
@@ -1,11 +1,49 @@
-import { defineComponent, reactive } from "vue";
+import { defineComponent, onMounted, reactive, ref } from "vue";
 import { PageHeader, PageContent } from "@/components/Page";
-import { Avatar, PageHeader as AntPageHeader, Statistic } from "ant-design-vue";
+import {
+  Avatar,
+  Button,
+  Card,
+  Col,
+  List,
+  PageHeader as AntPageHeader,
+  Row,
+  Statistic
+} from "ant-design-vue";
+import { PlusOutlined } from "@ant-design/icons-vue";
 import HeaderWrapper from "@/components/Page/HeaderWrapper";
 import style from "./style.module.less";
+import Radar from "@/components/Charts/Radar";
+
+interface ProjectItem {
+  id: number;
+  title: string;
+  cover: string;
+  description: string;
+}
+interface ActivitiesItem {
+  id: number;
+  avatar: string;
+  nickname: string;
+  project: string;
+  action: string;
+  event: string;
+  time: string;
+}
+
+interface TeamsItem {
+  id: number;
+  avatar: string;
+  name: string;
+}
 
 export default defineComponent({
   setup() {
+    const loading = ref(false);
+    const radarLoading = ref(false);
+    const projects = ref<ProjectItem[]>([]);
+    const activities = ref<ActivitiesItem[]>([]);
+    const teams = ref<TeamsItem[]>([]);
     const currentUser = reactive({
       avatar:
         "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
@@ -43,6 +81,100 @@ export default defineComponent({
       </div>
     );
 
+    onMounted(() => {
+      projects.value.push(
+        ...[
+          {
+            id: 1,
+            title: `Alipay`,
+            cover:
+              "https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png",
+            description: "那是一种内在的东西， 他们到达不了，也无法触及的"
+          },
+          {
+            id: 2,
+            title: `Angular`,
+            cover:
+              "https://gw.alipayobjects.com/zos/rmsportal/zOsKZmFRdUtvpqCImOVY.png",
+            description: "那是一种内在的东西， 他们到达不了，也无法触及的"
+          },
+          {
+            id: 3,
+            title: `Ant Design`,
+            cover:
+              "https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png",
+            description: "那是一种内在的东西， 他们到达不了，也无法触及的"
+          },
+          {
+            id: 4,
+            title: `Ant Design Pro`,
+            cover:
+              "https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png",
+            description: "那是一种内在的东西， 他们到达不了，也无法触及的"
+          },
+          {
+            id: 5,
+            title: `Bootstrap`,
+            cover:
+              "https://gw.alipayobjects.com/zos/rmsportal/siCrBXXhmvTQGWPNLBow.png",
+            description: "那是一种内在的东西， 他们到达不了，也无法触及的"
+          },
+          {
+            id: 6,
+            title: `Vue`,
+            cover:
+              "https://gw.alipayobjects.com/zos/rmsportal/ComBAopevLwENQdKWiIn.png",
+            description: "那是一种内在的东西， 他们到达不了，也无法触及的"
+          }
+        ]
+      );
+      for (let index = 0; index < 10; index++) {
+        activities.value.push({
+          id: index,
+          avatar: "",
+          nickname: `nickname-${index}`,
+          project: `project-${index}`,
+          action: `action-${index}`,
+          event: `event-${index}`,
+          time: `time-${index}`
+        });
+      }
+      teams.value.push(
+        ...[
+          {
+            id: 1,
+            avatar:
+              "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
+            name: "程序员"
+          },
+          {
+            id: 2,
+            avatar:
+              "https://gw.alipayobjects.com/zos/rmsportal/cnrhVkzwxjPwAaCfPbdc.png",
+            name: "板砖大队"
+          },
+          {
+            id: 3,
+            avatar:
+              "https://gw.alipayobjects.com/zos/rmsportal/gaOngJwsRYRaVAuXXcmB.png",
+            name: "福报企业"
+          },
+          {
+            id: 4,
+            avatar:
+              "https://gw.alipayobjects.com/zos/rmsportal/ubnKSIfAJTxIgXOKlciN.png",
+            name: "技术解决方案"
+          },
+          {
+            id: 5,
+            avatar:
+              "https://gw.alipayobjects.com/zos/rmsportal/WhxKECPNujWoWEFNdnJE.png",
+            name: "架构团队"
+          }
+        ]
+      );
+    });
+
     return () => (
       <>
         <PageHeader>
@@ -55,7 +187,135 @@ export default defineComponent({
             </HeaderWrapper>
           </AntPageHeader>
         </PageHeader>
-        <PageContent></PageContent>
+        <PageContent>
+          <div>
+            <Row gutter={24}>
+              <Col xl={16} lg={24} md={24} sm={24} xs={24}>
+                <Card
+                  class={style["project-list"]}
+                  loading={loading.value}
+                  style="margin-bottom: 24px;"
+                  bordered={false}
+                  title="进行中的项目"
+                  body-style={{ padding: 0 }}
+                  extra={<a>全部项目</a>}
+                >
+                  <div>
+                    {projects.value.map(item => (
+                      <Card.Grid
+                        class={style["project-card-grid"]}
+                        key={item.id}
+                      >
+                        <Card bordered={false} body-style={{ padding: 0 }}>
+                          <Card.Meta>
+                            {{
+                              title: () => (
+                                <div class={style["card-title"]}>
+                                  <Avatar size="small" src={item.cover} />
+                                  <a>{item.title}</a>
+                                </div>
+                              ),
+                              description: () => (
+                                <div class={style["card-description"]}>
+                                  {item.description}
+                                </div>
+                              )
+                            }}
+                          </Card.Meta>
+                          <div class={style["project-item"]}>
+                            <a href="/#/">科学搬砖组</a>
+                            <span class={style["datetime"]}>9小时前</span>
+                          </div>
+                        </Card>
+                      </Card.Grid>
+                    ))}
+                  </div>
+                </Card>
+                <Card loading={loading.value} title="动态" bordered={false}>
+                  <List>
+                    {activities.value.map(item => (
+                      <List.Item key={item.id}>
+                        <List.Item.Meta avatar={<Avatar src={item.avatar} />}>
+                          {{
+                            title: () => (
+                              <div>
+                                <span>{item.nickname}</span>&nbsp; 在&nbsp;
+                                <a href="#">{item.project}</a>&nbsp;
+                                <span>{item.action}</span>&nbsp;
+                                <a href="#">{item.event}</a>
+                              </div>
+                            ),
+                            description: () => <div>{item.time}</div>
+                          }}
+                        </List.Item.Meta>
+                      </List.Item>
+                    ))}
+                  </List>
+                </Card>
+              </Col>
+
+              <Col
+                style="padding: 0 12px"
+                xl={8}
+                lg={24}
+                md={24}
+                sm={24}
+                xs={24}
+              >
+                <Card
+                  title="快速开始 / 便捷导航"
+                  style="margin-bottom: 24px"
+                  bordered={false}
+                  body-style={{ padding: 0 }}
+                >
+                  <div class={style["item-group"]}>
+                    <a>操作一</a>
+                    <a>操作二</a>
+                    <a>操作三</a>
+                    <a>操作四</a>
+                    <a>操作五</a>
+                    <a>操作六</a>
+                    <Button
+                      size="small"
+                      type="primary"
+                      ghost
+                      icon={<PlusOutlined />}
+                    >
+                      添加
+                    </Button>
+                  </div>
+                </Card>
+                <Card
+                  title="XX 指数"
+                  style="margin-bottom: 24px"
+                  loading={radarLoading.value}
+                  bordered={false}
+                  body-style={{ padding: 0 }}
+                >
+                  <div style="min-height: 400px;">
+                    {/* :scale="scale" :axis1Opts="axis1Opts" :axis2Opts="axis2Opts" */}
+                    {/* <radar :data="radarData"/> */}
+                    <Radar />
+                  </div>
+                </Card>
+                <Card loading={loading.value} title="团队" bordered={false}>
+                  <div class={style["members"]}>
+                    <Row>
+                      {teams.value.map(item => (
+                        <Col key={item.id} span={12}>
+                          <a>
+                            <Avatar size="small" src={item.avatar} />
+                            <span class={style["member"]}>{item.name}</span>
+                          </a>
+                        </Col>
+                      ))}
+                    </Row>
+                  </div>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </PageContent>
       </>
     );
   }
