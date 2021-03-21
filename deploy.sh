a40b3774daab49d616d9717a4072d5659ab40d45
舒晓满 <shuxiaoman@xiaoman2020.local>
2021-03-21 14:31:10 +0800

    新增发布静态站点脚本

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build:av
+
+# navigate into the build output directory
+# cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
+
+yarn gh-pages -d dist
+
+# git init
+# git add -A
+# git commit -m 'deploy'
+
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:PanJiaChen/vue-element-admin.git master:gh-pages
+
+cd -
