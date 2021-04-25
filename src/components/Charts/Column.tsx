62a5aa051823093a3d3b7ea3a3b7fdd9410a982f
舒晓满 <shuxiaoman@xiaoman2020.local>
2021-04-25 14:57:58 +0800

    修复折线图更新数据错误问题

--- src/components/Charts/Column.tsx
+++ src/components/Charts/Column.tsx
@@ -23,20 +23,20 @@ export default defineComponent({
         {
           geometry: "column",
           isStack: true,
-          seriesField: "type",
-          label: {
-            // 可手动配置 label 数据标签位置
-            position: "middle", // 'top', 'bottom', 'middle'
-            // 可配置附加的布局方法
-            layout: [
-              // 柱形图数据标签位置自动调整
-              { type: "interval-adjust-position" },
-              // 数据标签防遮挡
-              { type: "interval-hide-overlap" },
-              // 数据标签文颜色自动调整
-              { type: "adjust-color" }
-            ]
-          }
+          seriesField: "type"
+          // label: {
+          //   // 可手动配置 label 数据标签位置
+          //   position: "middle", // 'top', 'bottom', 'middle'
+          //   // 可配置附加的布局方法
+          //   layout: [
+          //     // 柱形图数据标签位置自动调整
+          //     { type: "interval-adjust-position" },
+          //     // 数据标签防遮挡
+          //     { type: "interval-hide-overlap" },
+          //     // 数据标签文颜色自动调整
+          //     { type: "adjust-color" }
+          //   ]
+          // }
         },
         {
           geometry: "line",
@@ -54,7 +54,7 @@ export default defineComponent({
       chart.render();
     });
     watch(props.data, v => {
-      chart.changeData(v);
+      chart.changeData([v.column, v.line]);
     });
     return () => <div id="columnContainer"></div>;
   }
--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,9 +1,14 @@
-import { defineComponent, onMounted, ref } from "vue";
+import { defineComponent, onMounted, ref, watch } from "vue";
 import { getColumnData } from "@/services/dashboard/workspace";
 import { ColumnDataItem, LineDataItem } from "./data";
 import { PageContent } from "@/components/Page";
-import { Card } from "ant-design-vue";
+import {
+  Card,
+  Input
+  //  InputNumber
+} from "ant-design-vue";
 import ColumnChart from "@/components/Charts/Column";
+// import moment from "moment";
 
 const reducerOneDay = (list: ColumnDataItem[]) => {
   const obj: { [key: string]: number } = {};
@@ -21,6 +26,9 @@ const reducerOneDay = (list: ColumnDataItem[]) => {
   }));
 };
 
+const reducerAll = (list: ColumnDataItem[]) =>
+  list.reduce((i, j) => i + j.value, 0);
+
 export default defineComponent({
   setup() {
     const loading = ref(true);
@@ -29,6 +37,10 @@ export default defineComponent({
       line: []
     });
 
+    const password = ref("");
+
+    // const statisticData = ref<ColumnDataItem[]>([]);
+
     const loadData = async () => {
       const columnData = await getColumnData();
       data.value.column.splice(0, columnData.length, ...columnData);
@@ -36,14 +48,69 @@ export default defineComponent({
       loading.value = false;
     };
 
+    // const InputMount: (props: {
+    //   type: ColumnDataItem["type"];
+    // }) => JSX.Element[] = ({ type }) => {
+    //   const list = [];
+    //   const a = moment("2021-02-27");
+    //   while (a.format("YYYY-MM-DD") !== "2021-04-25") {
+    //     const b = a.format("YYYY-MM-DD");
+    //     list.push({
+    //       label: b
+    //     });
+    //     a.add(1, "days");
+    //   }
+    //   return list.map(i => (
+    //     <InputNumber
+    //       onChange={v => {
+    //         setData(i.label, type, v);
+    //       }}
+    //       style={{ width: "150px" }}
+    //       placeholder={i.label}
+    //     />
+    //   ));
+    // };
+
+    // const setData = (
+    //   date: string,
+    //   type: ColumnDataItem["type"],
+    //   value: number
+    // ) => {
+    //   const data = { date, type, value };
+    //   const index = statisticData.value.findIndex(i => i.date === date);
+    //   if (index === -1) {
+    //     console.log(22222);
+
+    //     statisticData.value.push(data);
+    //     return;
+    //   }
+    //   statisticData.value.splice(index, 1, data);
+    // };
+
     onMounted(() => {
-      loadData();
+      // loadData();
+      loading.value = false;
+    });
+
+    watch(password, v => {
+      if (btoa(v) === "MzMz") {
+        loadData();
+      }
     });
 
     return () => (
       <PageContent>
+        {/* <div>{JSON.stringify(statisticData.value)}</div>
+
+        <Card title="微信">
+          <InputMount type="wechat" />
+        </Card>
+        <Card title="支付宝">
+          <InputMount type="alipay" />
+        </Card> */}
+        <Input v-model={[password.value, "value"]} />
         <Card
-          title="营收"
+          title={`总计营收：${reducerAll([...data.value.column])}`}
           style={{ "margin-bottom": "24px" }}
           loading={loading.value}
           bordered={false}
